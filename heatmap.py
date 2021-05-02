5b21fc0d9b4bef126f3c8f98b02d82492125f0db
farmertheodor <deenerkeener@gmail.com>
2021-05-01 21:01:14 -0400

    created heatmap for chicken values

---
+++ heatmap.py
@@ -0,0 +1,14 @@
+import seaborn as sns; sns.set_theme()
+import numpy as np
+import matplotlib.pyplot as plt
+from math import floor
+
+
+def generate_heatmap(x_data, y_data,  file_name = None) -> None:
+	ax = sns.kdeplot(x=x_data, y=y_data,fill=True, thresh=0, levels=100, cmap="mako")
+	plt.show()
+
+if __name__ == '__main__':
+	x = np.random.uniform(low=1, high=128, size=(100,))
+	y = np.random.uniform(low=1, high=256, size=(100,))
+	generate_heatmap(x, y)
